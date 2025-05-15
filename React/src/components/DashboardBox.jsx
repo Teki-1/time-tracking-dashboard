b31594e19c2a4e375873fbf5d619ac3bb42439a2
Nour Nabarawi <nournabarawi20@gmail.com>
2025-05-15 18:04:25 +0300

    Added icons

--- React/src/components/DashboardBox.jsx
+++ React/src/components/DashboardBox.jsx
@@ -1,13 +1,26 @@
 import "react";
 
 export default function DashBoardBox(props) {
+  const { color, title, hours, duration, total, Icon } = props;
+
   return (
-    <div className="background-art" style={{ backgroundColor: props.color }}>
+    <div
+      className="background-art"
+      style={{ backgroundColor: color, position: "relative" }}
+    >
+      {Icon && (
+        <div
+          className="icon-wrapper"
+          style={{ position: "absolute", top: "1px", right: "10px" }}
+        >
+          <Icon height="50px" />
+        </div>
+      )}
       <div className="box-content">
-        <h1 className="title">{props.title}</h1>
-        <h2 className="current-total">{props.hours}</h2>
+        <h1 className="title">{title}</h1>
+        <h2 className="current-total">{hours}</h2>
         <p className="last-total">
-          Last {props.duration} - {props.total}
+          Last {duration} - {total}
         </p>
       </div>
     </div>
--- React/src/components/DashboardDisplay.jsx
+++ React/src/components/DashboardDisplay.jsx
@@ -1,6 +1,12 @@
 import DashBoardBox from "./DashboardBox";
 import time from "../data/time";
 import "../styles/dashboardbox.css";
+import Work from "../assets/images/icon-work.svg?react";
+import Study from "../assets/images/icon-study.svg?react";
+import Social from "../assets/images/icon-Social.svg?react";
+import SelfCare from "../assets/images/icon-self-care.svg?react";
+import Play from "../assets/images/icon-play.svg?react";
+import Excercise from "../assets/images/icon-exercise.svg?react";
 
 export default function DashboardDisplay() {
   return (
@@ -11,36 +17,47 @@ export default function DashboardDisplay() {
         hours={time[0].timeframes.weekly.current}
         duration="week"
         total={time[0].timeframes.weekly.previous}
+        Icon={Work}
       />
       <DashBoardBox
+        color="hsl(195, 74%, 62%)"
         title={time[1].title}
         hours={time[1].timeframes.weekly.current}
         duration="week"
         total={time[1].timeframes.weekly.previous}
+        Icon={Play}
       />
       <DashBoardBox
+        color="hsl(348, 100%, 68%)"
         title={time[2].title}
         hours={time[2].timeframes.weekly.current}
         duration="week"
         total={time[2].timeframes.weekly.previous}
+        Icon={Study}
       />
       <DashBoardBox
+        color="hsl(145, 58%, 55%)"
         title={time[3].title}
         hours={time[3].timeframes.weekly.current}
         duration="week"
         total={time[3].timeframes.weekly.previous}
+        Icon={Excercise}
       />
       <DashBoardBox
+        color="hsl(264, 64%, 52%)"
         title={time[4].title}
         hours={time[4].timeframes.weekly.current}
         duration="week"
         total={time[4].timeframes.weekly.previous}
+        Icon={Social}
       />
       <DashBoardBox
+        color="hsl(43, 84%, 65%)"
         title={time[5].title}
         hours={time[5].timeframes.weekly.current}
         duration="week"
         total={time[5].timeframes.weekly.previous}
+        Icon={SelfCare}
       />
     </div>
   );
